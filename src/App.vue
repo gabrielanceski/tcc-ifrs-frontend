3440c1bc130f90d64d19ba7f082dfc64f6861074
Felipe dos Santos Oliveira <felipedossantosads@gmail.com>
2024-10-10 11:38:50 -0300

    style: removendo comentários

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import GuestLayout from '@/components/layouts/SystemLayout.vue'
+import { RouterView } from 'vue-router'
+import SystemLayout from '@/components/layouts/SystemLayout.vue'
 import { onMounted, ref } from 'vue'
 import { hasToken } from '@/composables/auth.js'
 // import HelloWorld from './components/HelloWorld.vue'
@@ -16,20 +16,9 @@ function refreshData() {
 </script>
 
 <template>
-  <GuestLayout :is-logged-in="isLoggedIn" @refresh="refreshData">
+  <SystemLayout :is-logged-in="isLoggedIn" @refresh="refreshData">
     <RouterView @refresh="refreshData" />
-  </GuestLayout>
-<!--  <header>-->
-<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+  </SystemLayout>
 
-<!--    <div class="wrapper">-->
-<!--      <HelloWorld msg="You did it!" />-->
-
-<!--      <nav>-->
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
-<!--      </nav>-->
-<!--    </div>-->
-<!--  </header>-->
 
 </template>
